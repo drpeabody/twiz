d5a7e2ac2f644e24a412ad977a9a47443dd0af1a
Ashutosh Agarwal <3859208+radsaggi@users.noreply.github.com>
2024-12-05 00:38:16 -0800

    Update lib/question.dart with a responsive layout.
    Signed-off-by: Abhishek Agarwal <abhishek.h.agarwal@oracle.com>

--- lib/question.dart
+++ lib/question.dart
@@ -1,37 +1,42 @@
+import 'dart:math';
+
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
- 
+
 enum QuestionDisplayState {
   EMPTY,
   SHOW_CLUE1,
   SHOW_CLUE2,
 }
- 
+
 class QuestionState extends ChangeNotifier {
   var _displayState = QuestionDisplayState.EMPTY;
- 
+
   void doRevealClue1() {
     if (_displayState != QuestionDisplayState.SHOW_CLUE1) {
       _displayState = QuestionDisplayState.SHOW_CLUE1;
       notifyListeners();
     }
   }
- 
+
   void doRevealClue2() {
     if (_displayState != QuestionDisplayState.SHOW_CLUE2) {
       _displayState = QuestionDisplayState.SHOW_CLUE2;
       notifyListeners();
     }
   }
- 
+
   QuestionDisplayState getDisplayState() {
     return _displayState;
   }
 }
- 
+
+const QUESTION_DISPLAY_PADDING = 36.0;
+
 class QuestionDisplayWidget extends StatelessWidget {
   const QuestionDisplayWidget();
- 
+
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
@@ -40,63 +45,82 @@ class QuestionDisplayWidget extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.stretch,
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            Expanded(
-              flex: 3,
-              child: QuestionTitleWidget(),
-            ),
-            Expanded(flex: 6, child: ClueGridWidget()),
+            Expanded(flex: 3, child: QuestionTitleWidget()),
+            Expanded(flex: 8, child: ClueGridWidget()),
           ]),
     );
   }
 }
- 
+
 class QuestionTitleWidget extends StatelessWidget {
   const QuestionTitleWidget({
     super.key,
   });
- 
+
   @override
   Widget build(BuildContext context) {
     var question_state = context.watch<QuestionState>();
-    return Row(children: [
-      Expanded(
-        flex: 19,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Expanded(flex: 1, child: Text("Question Title here")),
-            Expanded(flex: 1, child: Text("Question description here")),
-          ],
+    var textTheme = Theme.of(context).textTheme;
+    var colorScheme = Theme.of(context).colorScheme;
+    return Container(
+      padding: EdgeInsets.all(QUESTION_DISPLAY_PADDING),
+      child: Row(children: [
+        Expanded(
+          flex: 19,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Expanded(
+                flex: 1,
+                child: AutoSizeText("Question Title here",
+                    maxLines: 1,
+                    minFontSize: textTheme.headlineLarge?.fontSize ?? 12,
+                    overflow: TextOverflow.fade,
+                    style: textTheme.displayLarge!
+                        .copyWith(color: colorScheme.primary)),
+              ),
+              Expanded(
+                flex: 2,
+                child: AutoSizeText(
+                    "Question description here. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                    maxLines: 3,
+                    minFontSize: textTheme.headlineLarge?.fontSize ?? 12,
+                    overflow: TextOverflow.ellipsis,
+                    style: textTheme.displayMedium!
+                        .copyWith(color: colorScheme.secondary)),
+              ),
+            ],
+          ),
         ),
-      ),
-      Expanded(
-        flex: 1,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            IconButton.filledTonal(
-              onPressed: () {
-                question_state.doRevealClue1();
-              },
-              icon: Icon(Icons.favorite),
-            ),
-            IconButton.filledTonal(
-              onPressed: () {
-                question_state.doRevealClue2();
-              },
-              icon: Icon(Icons.favorite),
-            ),
-          ],
+        Expanded(
+          flex: 1,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              IconButton.filledTonal(
+                onPressed: () {
+                  question_state.doRevealClue1();
+                },
+                icon: Icon(Icons.favorite),
+              ),
+              IconButton.filledTonal(
+                onPressed: () {
+                  question_state.doRevealClue2();
+                },
+                icon: Icon(Icons.favorite),
+              ),
+            ],
+          ),
         ),
-      ),
-    ]);
+      ]),
+    );
   }
 }
- 
+
 class ClueGridWidget extends StatelessWidget {
   const ClueGridWidget({super.key});
- 
+
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(builder: (context, constraints) {
@@ -108,37 +132,45 @@ class ClueGridWidget extends StatelessWidget {
           padding: const EdgeInsets.all(8),
           childAspectRatio:
               (6.0 / 3.0) * (constraints.maxWidth / constraints.maxHeight),
-          children: List.generate(18, (idx) => buildClueButton(context, idx),
+          children: List.generate(
+              18, (idx) => buildClueGridWidget(context, idx),
               growable: false),
         ),
       );
     });
   }
- 
-  Widget buildClueButton(BuildContext context, int index) {
+
+  Widget buildClueGridWidget(BuildContext context, int index) {
     if (index < 3) {
       return Container(
         margin: const EdgeInsets.all(12.0),
-        decoration: clueButtonShapeDecoration(Theme.of(context).colorScheme),
+        decoration: _clueButtonDecoration(Theme.of(context).colorScheme),
         alignment: Alignment.center,
         child: ClueScoreText(columnIndex: index),
       );
     } else {
       var clueIdx = index - 3;
       clueIdx = 5 * (clueIdx % 3) + (clueIdx ~/ 3);
-      return ClueButtonWidget(index: clueIdx);
+      return ClueDisplayWidget(index: clueIdx);
     }
   }
 }
- 
+
+ShapeDecoration _clueButtonDecoration(ColorScheme colorScheme) {
+  return ShapeDecoration(
+    shape: StadiumBorder(),
+    color: colorScheme.primaryContainer,
+  );
+}
+
 const CLUE1_SCORES = [20, 30, 40];
 const CLUE2_SCORES = [10, 15, 20];
- 
+
 class ClueScoreText extends StatelessWidget {
   const ClueScoreText({super.key, required this.columnIndex});
- 
+
   final int columnIndex;
- 
+
   @override
   Widget build(BuildContext context) {
     var question_state = context.watch<QuestionState>();
@@ -147,40 +179,146 @@ class ClueScoreText extends StatelessWidget {
       QuestionDisplayState.SHOW_CLUE1 => "${CLUE1_SCORES[columnIndex]} points",
       QuestionDisplayState.SHOW_CLUE2 => "${CLUE2_SCORES[columnIndex]} points",
     };
- 
-    return Text(score_text);
+
+    var theme = Theme.of(context);
+    return AutoSizeText(score_text,
+        maxLines: 1,
+        minFontSize: theme.textTheme.headlineSmall?.fontSize ?? 12,
+        style: theme.textTheme.headlineLarge?.copyWith(
+            fontWeight: FontWeight.bold,
+            color: theme.colorScheme.onPrimaryContainer),
+        overflow: TextOverflow.ellipsis);
   }
 }
- 
-class ClueButtonWidget extends StatefulWidget {
-  const ClueButtonWidget({super.key, required this.index});
- 
+
+const CLUE_DISPLAY_PADDING = 12.0;
+
+class ClueDisplayWidget extends StatefulWidget {
+  const ClueDisplayWidget({super.key, required this.index});
+
   final int index;
- 
+
   @override
-  State<ClueButtonWidget> createState() => _ClueButtonWidgetState();
+  State<ClueDisplayWidget> createState() => _ClueDisplayWidgetState();
 }
- 
-class _ClueButtonWidgetState extends State<ClueButtonWidget> {
+
+class _ClueDisplayWidgetState extends State<ClueDisplayWidget> {
+  var answer_shown = false;
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: const EdgeInsets.all(12.0),
-      decoration: clueButtonShapeDecoration(Theme.of(context).colorScheme),
-      alignment: Alignment.center,
-      child: Text("Clue ${widget.index} goes here"),
+      margin: const EdgeInsets.all(CLUE_DISPLAY_PADDING),
+      child: LayoutBuilder(builder: (context, constraints) {
+        var maxIconSize = min(constraints.maxHeight, constraints.maxWidth);
+        return Stack(
+          alignment: Alignment.centerLeft,
+          fit: StackFit.expand,
+          children: [
+            Positioned(
+              width: constraints.maxWidth,
+              height: constraints.maxHeight,
+              child:
+                  ClueTextWidget(index: widget.index, iconPadding: maxIconSize),
+            ),
+            Positioned(
+              width: maxIconSize,
+              height: maxIconSize,
+              child: ClueButtonWidget(index: widget.index, size: maxIconSize),
+            ),
+          ],
+        );
+      }),
     );
   }
 }
- 
-ShapeDecoration clueButtonShapeDecoration(ColorScheme colorScheme) {
-  return ShapeDecoration(
-    shape: RoundedRectangleBorder(
-      borderRadius: BorderRadius.circular(48.0),
-    ),
-    gradient: RadialGradient(
-      colors: [colorScheme.primaryContainer, colorScheme.secondaryContainer],
-    ),
-  );
+
+class ClueButtonWidget extends StatelessWidget {
+  const ClueButtonWidget({super.key, required this.index, required this.size});
+
+  final int index;
+  final double size;
+
+  @override
+  Widget build(BuildContext context) {
+    var theme = Theme.of(context);
+    return FilledButton(
+      onPressed: () {},
+      child: AutoSizeText("${this.index}",
+          maxLines: 1,
+          textScaleFactor: 1.6,
+          minFontSize: theme.textTheme.headlineSmall?.fontSize ?? 12,
+          style: theme.textTheme.headlineLarge?.copyWith(
+              color: theme.colorScheme.onPrimaryContainer),
+          overflow: TextOverflow.visible),
+      style: FilledButton.styleFrom(
+          textStyle: theme.textTheme.headlineLarge
+              ?.copyWith(color: theme.colorScheme.onInverseSurface),
+          fixedSize: Size.square(this.size),
+          backgroundColor: theme.colorScheme.inversePrimary,
+          elevation: 0,
+          shape: const CircleBorder()),
+    );
+  }
 }
- 
+
+class ClueTextWidget extends StatelessWidget {
+  final int index;
+  final double iconPadding;
+
+  const ClueTextWidget(
+      {super.key, required this.index, required this.iconPadding});
+
+  @override
+  Widget build(BuildContext context) {
+    var question_state = context.watch<QuestionState>();
+    var clue_text = switch (question_state.getDisplayState()) {
+      QuestionDisplayState.EMPTY => "",
+      QuestionDisplayState.SHOW_CLUE1 => "Clue ${this.index} Hint 1. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      QuestionDisplayState.SHOW_CLUE2 => "Clue ${this.index} Hint 2. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    };
+
+    var theme = Theme.of(context);
+    return
+        // AnimatedSwitcher(
+        //   duration: Duration(milliseconds: 200),
+        //   transitionBuilder: __transitionBuilder,
+        //   layoutBuilder: (widget, list) => Stack(children: [widget!, ...list]),
+        //   switchInCurve: Curves.easeInBack,
+        //   switchOutCurve: Curves.easeInBack.flipped,
+        //   child:
+        Container(
+      padding: EdgeInsets.only(left: iconPadding + 2 * CLUE_DISPLAY_PADDING, right: CLUE_DISPLAY_PADDING),
+      alignment: Alignment.centerLeft,
+      decoration: _clueButtonDecoration(theme.colorScheme),
+      child: AutoSizeText(clue_text,
+          maxLines: 3,
+          key: ValueKey(question_state.getDisplayState()),
+          minFontSize: theme.textTheme.headlineSmall?.fontSize ?? 12,
+          style: theme.textTheme.headlineLarge
+              ?.copyWith(color: theme.colorScheme.onPrimaryContainer),
+          overflow: TextOverflow.ellipsis),
+      // ),
+    );
+  }
+
+  // Widget __transitionBuilder(Widget widget, Animation<double> animation) {
+  //   final rotateAnim = Tween(begin: pi, end: 0.0).animate(animation);
+  //   return AnimatedBuilder(
+  //     animation: rotateAnim,
+  //     child: widget,
+  //     builder: (context, widget) {
+  //       final isUnder = false;
+  //       var tilt = ((animation.value - 0.5).abs() - 0.5) * 0.003;
+  //       tilt *= isUnder ? -1.0 : 1.0;
+  //       final value =
+  //           isUnder ? min(rotateAnim.value, pi / 2) : rotateAnim.value;
+  //       return Transform(
+  //         transform: Matrix4.rotationX(value)..setEntry(3, 0, tilt),
+  //         child: widget,
+  //         alignment: Alignment.center,
+  //       );
+  //     },
+  //   );
+  // }
+}
