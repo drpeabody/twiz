40d29274fb964c7789a91485cee519962e2d7869
drpeabody <abhiagar97@gmail.com>
2025-01-14 04:12:38 +0530

    Added Basic Styling to team options popup

--- lib/widgets/team_options.dart
+++ lib/widgets/team_options.dart
@@ -98,136 +98,171 @@ class __TeamIndexCounterState extends State<_TeamIndexCounter> {
 
         final displayCharacterstics = context.read<DisplayCharacterstics>();
         final scoreboardState = context.watch<GlobalScoreboard>();
+        final textTheme = Theme.of(context).textTheme;
 
-        return Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-                Text("Press the Button to apply"),
-                ElevatedButton(
-                    child: Text("this color"),
-                    style: ElevatedButton.styleFrom(
-                        backgroundColor: pickerColor
-                    ),
-                    onPressed: () => showDialog(
-                        context: context,
-                        builder: (BuildContext context) {
-                            return AlertDialog(
-                                title: const Text('Pick a color!'),
-                                content: SingleChildScrollView(
-                                child: ColorPicker(
-                                    pickerColor: pickerColor,
-                                    onColorChanged: changeColor,
-                                ),
-                                // Use Material color picker:
-                                //
-                                // child: MaterialPicker(
-                                //   pickerColor: pickerColor,
-                                //   onColorChanged: changeColor,
-                                //   showLabel: true, // only on portrait mode
-                                // ),
-                                //
-                                // Use Block color picker:
-                                //
-                                // child: BlockPicker(
-                                //   pickerColor: currentColor,
-                                //   onColorChanged: changeColor,
-                                // ),
-                                //
-                                // child: MultipleChoiceBlockPicker(
-                                //   pickerColors: currentColors,
-                                //   onColorsChanged: changeColors,
-                                // ),
-                                ),
-                                actions: <Widget>[
-                                    ElevatedButton(
-                                        child: const Text('Got it'),
-                                        onPressed: () {
-                                            setState(() => currentColor = pickerColor);
-                                            Navigator.of(context).pop();
-                                        },
-                                    ),
-                                ],
-                            );
-                        }
-                    )
-                ),
-                Text("and the Team Name"),
-                Material( child: TextFormField(
-                    decoration: const InputDecoration(hintText: 'Name'),
-                    // validator: (value) {
-                    //     if (value == '') {
-                    //         return '*Required';
-                    //     }
-                    //     print(value?.toString());
-                    //     for(int i = 0 ; i < ScoreboardLength; i++) {
-                    //         print(scoreboardState.getTeamName(i));
-                    //         if(scoreboardState.getTeamName(i) == value) {
-                    //             return 'Name is already taken!';
-                    //         }
-                    //     }
-                    //     return null;
-                    // },
-                    onChanged: (value) {
-                        setState(() {
-                            if (value != '') {
-                                _currentTeamName = value;
-                            }
-                        });
-                    }
-                )),
-                Text("for the team at position "),
-                Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
+        return Container(
+            color: Color(0xdddddd33),
+            child: SizedBox(
+                width: 1000.0,
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
-                        IconButton.filledTonal(
-                            onPressed: () {
-                                setState(() {
-                                    if(_index > 0) {
-                                        _index--;
-                                    }
-                                });
-                            },
-                            icon: Icon(Icons.flourescent),
-                            iconSize: displayCharacterstics.iconSize,
-                            padding: displayCharacterstics.fullPadding / 2,
-                        ),
-                        AnimatedOpacity(
-                            opacity: (ScoreboardLength + _index) / (2.0 * ScoreboardLength),
-                            duration: Duration(milliseconds: 500),
-                            child: Text(
-                                '${_index+1}',
-                                style: TextStyle(
-                                    fontSize: 48.0,
-                                    color: Colors.white,
-                                ),
+                        Text("Press the Button to apply",
+                            maxLines: 3,
+                            overflow: TextOverflow.ellipsis,
+                            style: textTheme.displayMedium
+                                ?.apply(color: Colors.black, fontWeightDelta: 3),
+                            textScaler: displayCharacterstics.textScaler,
+                            textAlign: TextAlign.center),
+                        ElevatedButton(
+                            child: Text("this color"),
+                            style: ElevatedButton.styleFrom(
+                                backgroundColor: pickerColor
                             ),
+                            onPressed: () => showDialog(
+                                context: context,
+                                builder: (BuildContext context) {
+                                    return AlertDialog(
+                                        title: const Text('Pick a color!'),
+                                        content: SingleChildScrollView(
+                                        child: ColorPicker(
+                                            pickerColor: pickerColor,
+                                            onColorChanged: changeColor,
+                                        ),
+                                        // Use Material color picker:
+                                        //
+                                        // child: MaterialPicker(
+                                        //   pickerColor: pickerColor,
+                                        //   onColorChanged: changeColor,
+                                        //   showLabel: true, // only on portrait mode
+                                        // ),
+                                        //
+                                        // Use Block color picker:
+                                        //
+                                        // child: BlockPicker(
+                                        //   pickerColor: currentColor,
+                                        //   onColorChanged: changeColor,
+                                        // ),
+                                        //
+                                        // child: MultipleChoiceBlockPicker(
+                                        //   pickerColors: currentColors,
+                                        //   onColorsChanged: changeColors,
+                                        // ),
+                                        ),
+                                        actions: <Widget>[
+                                            ElevatedButton(
+                                                child: const Text('Got it'),
+                                                onPressed: () {
+                                                    setState(() => currentColor = pickerColor);
+                                                    Navigator.of(context).pop();
+                                                },
+                                            ),
+                                        ],
+                                    );
+                                }
+                            )
+                        ),
+                        Text("and the Team Name",
+                            maxLines: 3,
+                            overflow: TextOverflow.ellipsis,
+                            style: textTheme.displayMedium
+                                ?.apply(color: Colors.black, fontWeightDelta: 3),
+                            textScaler: displayCharacterstics.textScaler,
+                            textAlign: TextAlign.center),
+                        Material( child: SizedBox(
+                            width: 500.0,
+                            child: TextFormField(
+                                style: TextStyle(fontSize: 40.0, height: 2.0, color: Colors.black),
+                                decoration: const InputDecoration(hintText: 'Name'),
+                                // validator: (value) {
+                                //     if (value == '') {
+                                //         return '*Required';
+                                //     }
+                                //     print(value?.toString());
+                                //     for(int i = 0 ; i < ScoreboardLength; i++) {
+                                //         print(scoreboardState.getTeamName(i));
+                                //         if(scoreboardState.getTeamName(i) == value) {
+                                //             return 'Name is already taken!';
+                                //         }
+                                //     }
+                                //     return null;
+                                // },
+                                onChanged: (value) {
+                                    setState(() {
+                                        if (value != '') {
+                                            _currentTeamName = value;
+                                        }
+                                    });
+                                }
+                            )
+                        )),
+                        Text("for the team at position ",
+                            maxLines: 3,
+                            overflow: TextOverflow.ellipsis,
+                            style: textTheme.displayMedium
+                                ?.apply(color: Colors.black, fontWeightDelta: 3),
+                            textScaler: displayCharacterstics.textScaler,
+                            textAlign: TextAlign.center),
+                        Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                                const Spacer(flex: 10),
+                                IconButton.filledTonal(
+                                    onPressed: () {
+                                        setState(() {
+                                            if(_index > 0) {
+                                                _index--;
+                                            }
+                                        });
+                                    },
+                                    icon: Icon(Icons.flourescent),
+                                    iconSize: displayCharacterstics.iconSize,
+                                    padding: displayCharacterstics.fullPadding / 2,
+                                ),
+                                const Spacer(flex: 1),
+                                AnimatedOpacity(
+                                    opacity: (ScoreboardLength + _index) / (2.0 * ScoreboardLength),
+                                    duration: Duration(milliseconds: 500),
+                                    child: Text(
+                                        '${_index+1}',
+                                        maxLines: 3,
+                                        overflow: TextOverflow.ellipsis,
+                                        style: textTheme.displayMedium
+                                            ?.apply(color: Colors.black, fontWeightDelta: 3),
+                                        textScaler: displayCharacterstics.textScaler,
+                                        textAlign: TextAlign.center,
+                                    ),
+                                ),
+                                const Spacer(flex: 1),
+                                IconButton.filledTonal(
+                                    onPressed: () {
+                                        setState(() {
+                                            if(_index < ScoreboardLength - 1) {
+                                                _index++;
+                                            }
+                                        });
+                                    },
+                                    icon: Icon(Icons.add),
+                                    iconSize: displayCharacterstics.iconSize,
+                                    padding: displayCharacterstics.fullPadding / 2,
+                                ),
+                                const Spacer(flex: 10),
+                            ]
                         ),
                         IconButton.filledTonal(
                             onPressed: () {
-                                setState(() {
-                                    if(_index < ScoreboardLength - 1) {
-                                        _index++;
-                                    }
-                                });
+                                if(_index < ScoreboardLength && _index > -1) {
+                                    scoreboardState.updateColor(currentColor, _index);
+                                    scoreboardState.updateName(_currentTeamName, _index);
+                                }
                             },
-                            icon: Icon(Icons.add),
+                            icon: Icon(Icons.keyboard_return),
                             iconSize: displayCharacterstics.iconSize,
                             padding: displayCharacterstics.fullPadding / 2,
                         ),
-                    ]
-                ),
-                IconButton.filledTonal(
-                    onPressed: () {
-                        if(_index < ScoreboardLength && _index > -1) {
-                            scoreboardState.updateColor(currentColor, _index);
-                            scoreboardState.updateName(_currentTeamName, _index);
-                        }
-                    },
-                    icon: Icon(Icons.keyboard_return),
-                    iconSize: displayCharacterstics.iconSize,
-                    padding: displayCharacterstics.fullPadding / 2,
-                ),
-            ],
+                    ],
+                )
+            )
         );
     }
 }
